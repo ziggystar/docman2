920c63f290b5b72766e14f77b0dbac91f4730a86
thomas <ziyggstar@gmx.de>
2014-01-03 10:21:09 +0100

    add quick search bar

--- src/main/scala/docman/Main.scala
+++ src/main/scala/docman/Main.scala
@@ -2,8 +2,11 @@ package docman
 
 import java.io.{FilenameFilter, File}
 import scala.swing._
-import scala.swing.event.TableRowsSelected
+import scala.swing.event.{ValueChanged, TableRowsSelected}
 import javax.swing.event.{ListSelectionEvent, ListSelectionListener}
+import javax.swing.{RowFilter, DefaultRowSorter, JLabel, JTextField}
+import migpanel.MigPanel
+import javax.swing.RowFilter.Entry
 
 /**
  * @author Thomas Geier
@@ -30,6 +33,17 @@ object Main extends Reactor {
       }
     })
 
+    val quickSearchBar = new TextField(30)
+    this.listenTo(quickSearchBar)
+    reactions += {
+      case ValueChanged(_) => table.getRowSorter.asInstanceOf[DefaultRowSorter[DocumentTableModel,Int]].setRowFilter(
+        new RowFilter[DocumentTableModel,Int]{
+          def include(entry: Entry[_ <: DocumentTableModel, _ <: Int]): Boolean = 
+            Seq(2,3).exists(c => entry.getStringValue(c).toLowerCase.contains(quickSearchBar.text.toLowerCase))
+        }
+      )
+    }
+
     val menuB = new MenuBar {
       contents += new Menu("File") {
         contents += new MenuItem(Action("Save All Meta"){tableModel.saveAllMeta()})
@@ -41,10 +55,15 @@ object Main extends Reactor {
       }
     }
 
+    val leftPane = new MigPanel
+    leftPane.add(Component.wrap(new JLabel("Filter")),"")
+    leftPane.add(quickSearchBar,"wrap")
+    leftPane.add(new ScrollPane(Component.wrap(table)),"span 2")
+
     val frame = new MainFrame{
       menuBar = menuB
       minimumSize = new Dimension(800,600)
-      contents = new SplitPane(Orientation.Vertical,new ScrollPane(Component.wrap(table)),viewer)
+      contents = new SplitPane(Orientation.Vertical,leftPane,viewer)
     }
 
     frame.open()
