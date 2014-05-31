921d28586e9987b4074a2570982fe8b548ce33f5
Thomas Geier <ziggystar@gmx.de>
2014-05-31 16:56:35 +0200

    moved application into case class
     - added dev mode to work with different preferences

--- src/main/scala/docman/Main.scala
+++ src/main/scala/docman/Main.scala
@@ -23,10 +23,7 @@ import java.text.DateFormat
  * @since 9/8/13
  */
 
-object Main extends Reactor {
-
-  val preferences: Preferences = Preferences.userNodeForPackage(this.getClass)
-
+case class AppMain(preferences: Preferences) extends Reactor {
   val applicationTitle = "Docman2"
   val versionString = "1.0"
 
@@ -35,7 +32,7 @@ object Main extends Reactor {
     name = "dbDirs"
   )
 
-  val saveDBDie = dbDirs.foreach{ dirs =>
+  val saveDBDie: Obs = dbDirs.foreach{ dirs =>
     preferences.put("db.dirs", dirs.mkString(";"))
   }
 
@@ -53,7 +50,7 @@ object Main extends Reactor {
   }
 
   val tableModel: DocumentTableModel = DocumentTableModel(docs(), DProp.ALL)
-  val docUpdate = docs.foreach(tableModel.setDocs)
+  val docUpdate: Obs = docs.foreach(tableModel.setDocs)
 
   def showPreferenceDialog(owner: Window): Unit = {
     val dia = new Dialog(owner)
@@ -88,70 +85,78 @@ object Main extends Reactor {
     dia.open()
   }
 
+  val viewer: PDFViewer = new PDFViewer
+
+  val table: DocumentTable = new DocumentTable(tableModel)
+
+  table.getSelectionModel.addListSelectionListener(new ListSelectionListener {
+    def valueChanged(e: ListSelectionEvent): Unit = {
+      if(!e.getValueIsAdjusting)
+        table.getSelectedDocuments.headOption.foreach(d => viewer.setFile(Some(d.pdfFile)))
+    }
+  })
+
+  val quickSearchBar: TextField = new TextField(30)
+  this.listenTo(quickSearchBar)
+  reactions += {
+    case ValueChanged(_) => table.getRowSorter.asInstanceOf[DefaultRowSorter[DocumentTableModel,Int]].setRowFilter(
+      new RowFilter[DocumentTableModel,Int]{
+        def include(entry: Entry[_ <: DocumentTableModel, _ <: Int]): Boolean =
+          Seq(2,3).exists(c => entry.getStringValue(c).toLowerCase.contains(quickSearchBar.text.toLowerCase))
+      }
+    )
+  }
+
+  val leftPane = new MigPanel("fill","","[10]10[fill]")
+  leftPane.add(Component.wrap(new JLabel("Filter")),"")
+  leftPane.add(quickSearchBar,"wrap")
+  val scrollPane: ScrollPane = new ScrollPane(Component.wrap(table))
+  leftPane.add(scrollPane,"growx, pushy, span 2")
+
+  val frame = new MainFrame{
+    val icon = ImageIO.read(this.getClass.getClassLoader.getResourceAsStream("images/app_icon.png"))
+    iconImage = icon
+    title = f"Docman - $versionString"
+
+    minimumSize = new Dimension(800,600)
+    private val splitPane: SplitPane = new SplitPane(Orientation.Vertical, leftPane, viewer)
+    splitPane.resizeWeight = 0.6
+    contents = splitPane
+
+    override def closeOperation() {
+      visible = true
+      tableModel.saveAllMeta()
+      super.closeOperation()
+    }
+  }
+
+
+  val menuB = new MenuBar {
+    contents += new Menu("File") {
+      contents += new MenuItem(Action("Save All Meta"){tableModel.saveAllMeta()})
+      contents += new MenuItem(Action("Open Selected PDF"){
+        table.getSelectedDocuments.headOption.foreach(selectedPDF =>
+          sys.process.Process(f"gnome-open ${selectedPDF.pdfFile.getAbsoluteFile}").run())
+      })
+      contents += new MenuItem(Action("Preferences")(showPreferenceDialog(frame)))
+      contents += new MenuItem(Action("Exit"){frame.closeOperation()})
+    }
+    contents += new Menu("About") {
+      contents += new MenuItem(Action("About")(showAboutDialog(frame)))
+    }
+  }
+  frame.menuBar = menuB
+  frame.open()
+}
+
+object Main{
   def main(args: Array[String]) {
-    val viewer = new PDFViewer
-
-    val table = new DocumentTable(tableModel)
-
-    table.getSelectionModel.addListSelectionListener(new ListSelectionListener {
-      def valueChanged(e: ListSelectionEvent): Unit = {
-        if(!e.getValueIsAdjusting)
-          table.getSelectedDocuments.headOption.foreach(d => viewer.setFile(Some(d.pdfFile)))
-      }
-    })
-
-    val quickSearchBar = new TextField(30)
-    this.listenTo(quickSearchBar)
-    reactions += {
-      case ValueChanged(_) => table.getRowSorter.asInstanceOf[DefaultRowSorter[DocumentTableModel,Int]].setRowFilter(
-        new RowFilter[DocumentTableModel,Int]{
-          def include(entry: Entry[_ <: DocumentTableModel, _ <: Int]): Boolean = 
-            Seq(2,3).exists(c => entry.getStringValue(c).toLowerCase.contains(quickSearchBar.text.toLowerCase))
-        }
-      )
-    }
-
-    val leftPane = new MigPanel("fill","","[10]10[fill]")
-    leftPane.add(Component.wrap(new JLabel("Filter")),"")
-    leftPane.add(quickSearchBar,"wrap")
-    val scrollPane: ScrollPane = new ScrollPane(Component.wrap(table))
-    leftPane.add(scrollPane,"growx, pushy, span 2")
-
-    val frame = new MainFrame{
-      val icon = ImageIO.read(this.getClass.getClassLoader.getResourceAsStream("images/app_icon.png"))
-      iconImage = icon
-      title = f"Docman - $versionString"
-
-      minimumSize = new Dimension(800,600)
-      private val splitPane: SplitPane = new SplitPane(Orientation.Vertical, leftPane, viewer)
-      splitPane.resizeWeight = 0.6
-      contents = splitPane
-
-      override def closeOperation() {
-        visible = true
-        tableModel.saveAllMeta()
-        super.closeOperation()
-      }
-    }
-
-
-    val menuB = new MenuBar {
-      contents += new Menu("File") {
-        contents += new MenuItem(Action("Save All Meta"){tableModel.saveAllMeta()})
-        contents += new MenuItem(Action("Open Selected PDF"){
-          table.getSelectedDocuments.headOption.foreach(selectedPDF =>
-            sys.process.Process(f"gnome-open ${selectedPDF.pdfFile.getAbsoluteFile}").run())
-        })
-        contents += new MenuItem(Action("Preferences")(showPreferenceDialog(frame)))
-        contents += new MenuItem(Action("Exit"){frame.closeOperation()})
-      }
-      contents += new Menu("About") {
-        contents += new MenuItem(Action("About")(showAboutDialog(frame)))
-      }
-    }
-
-    frame.menuBar = menuB
-
-    frame.open()
+    val devMode = args.exists(_ == "--dev")
+    val prefs: Preferences =
+      if(devMode) Preferences.userNodeForPackage(this.getClass).node("development")
+      else        Preferences.userNodeForPackage(this.getClass)
+    val app = AppMain(
+      preferences = prefs
+    )
   }
 }
