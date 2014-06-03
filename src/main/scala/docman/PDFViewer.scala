3df0b78b9b76ecb75ac3c13f80d7a0117f07adcc
Thomas Geier <ziggystar@gmx.de>
2014-06-03 22:22:32 +0200

    Resource Cache

--- src/main/scala/docman/PDFViewer.scala
+++ src/main/scala/docman/PDFViewer.scala
@@ -1,15 +1,14 @@
 package docman
 
-import resource._
 import java.io.File
 import scala.swing.{Button, Dimension, Component}
 import java.awt.image.BufferedImage
 import java.awt.Image
 import org.apache.pdfbox.pdmodel.{PDPage, PDDocument}
 import rx._
-import rx.ops._
 import migpanel.MigPanel
 import rxutils.swing.RxLabel
+import utils.ResourceCache
 
 /**
  * @author Thomas Geier
@@ -18,13 +17,12 @@ import rxutils.swing.RxLabel
 class PDFView(val pdfFile: Rx[Option[File]], val displayedPage: Rx[Int]) extends Component {
   minimumSize = new Dimension(300,400)
 
-  val pageAsImage: Rx[Option[Image]] = Rx{pdfFile().map(PDFViewer.renderPDFPage(_,displayedPage()))}
-
-  val imageChanged = Obs(pageAsImage)(this.repaint())
+  val pdfAndPage = Rx{(pdfFile(),displayedPage())}
+  val imageChanged = Obs(pdfAndPage)(this.repaint())
 
   override protected def paintComponent(g: swing.Graphics2D) {
     g.clearRect(g.getClipBounds.x,g.getClipBounds.y,g.getClipBounds.width,g.getClipBounds.height)
-    for(img <- pageAsImage()){
+    for(img <- pdfFile().map(PDFViewer.renderPDFPage(_,displayedPage()))){
       val (imgW,imgH) = (img.getWidth(this.peer), img.getHeight(this.peer))
       val (cW,cH) = (this.peer.getWidth, this.peer.getHeight)
       val scale = math.min(cW / imgW.toDouble, cH / imgH.toDouble)
@@ -52,14 +50,13 @@ object PDFViewer{
     }
   }
 
-  def getNumPages(file: File): Int =
-    managed(PDDocument.load(file)).map(pd =>
-      pd.getNumberOfPages
-    ).opt.getOrElse(0)
+  private val documentCache: ResourceCache[File, PDDocument] = new ResourceCache(PDDocument.load(_: File))
+
+  def getNumPages(file: File): Int = documentCache.get(file)(_.getNumberOfPages).getOrElse(0)
 
   def renderPDFPage(file: File, page: Int, dpi: Int = 150): Image = {
-    managed(PDDocument.load(file)).map(pd =>
+    documentCache.get(file)(pd =>
       pd.getDocumentCatalog.getAllPages.get(page).asInstanceOf[PDPage].convertToImage(BufferedImage.TYPE_BYTE_GRAY, dpi)
-    ).opt.getOrElse(new BufferedImage(1,1,BufferedImage.TYPE_BYTE_GRAY))
+    ).getOrElse(new BufferedImage(1,1,BufferedImage.TYPE_BYTE_GRAY))
   }
 }
---
+++ src/main/scala/utils/ResourceCache.scala
@@ -0,0 +1,35 @@
+package utils
+
+import scala.util.Try
+
+/** Manages a pool of opened resources. Currently it's hard to prevent escaping of references. Maybe the return type
+  * of `get` should be changed. Also async operations might be useful.
+  * @author Thomas Geier
+  */
+class ResourceCache[K,V <: {def close(): Unit}](val open: K => V, val size: Int = 3) {
+  private val cache = new java.util.concurrent.ConcurrentHashMap[K,Try[V]]
+  private val lastAccessed =  new java.util.concurrent.ConcurrentHashMap[K,Long]
+  @volatile private var lastTime = 0
+
+  def get[A](k: K)(f: V => A): Try[A] = this.synchronized{
+    val existingKey: Option[K] = Some(k).filter(cache.containsKey)
+    val v: Try[V] = existingKey.fold(create(k))(cache.get)
+    lastAccessed.put(k, lastTime)
+    lastTime = lastTime + 1
+    v.map(f)
+  }
+
+  private def create(k: K): Try[V] = {
+    if(cache.size == size){
+      import collection.JavaConverters._
+      val toRemove: K = lastAccessed.entrySet.asScala.minBy(_.getValue).getKey
+      cache.get(toRemove).foreach(_.close())
+      cache.remove(toRemove)
+      lastAccessed.remove(toRemove)
+    }
+
+    val newV = Try(open(k))
+    cache.put(k, newV)
+    newV
+  }
+}
