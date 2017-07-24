4a17e6a36ad43a79b4aba9e1099a12d02c9073e9
Thomas Vultura <ziggystar@gmx.de>
2017-07-24 20:10:02 +0200

    fix bug in displaying correct pdf pages

--- src/main/scala/docman/components/pdf/PDFViewer.scala
+++ src/main/scala/docman/components/pdf/PDFViewer.scala
@@ -4,6 +4,7 @@ import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.File
 
+import com.typesafe.scalalogging.slf4j.StrictLogging
 import docman.components.{MigPanel, RxLabel}
 import org.apache.pdfbox.pdmodel.{PDDocument, PDPage}
 import rx.lang.scala._
@@ -16,19 +17,20 @@ import scala.swing.{Button, Component, Dimension}
  * @author Thomas Geier
  * @since 12/15/13
  */
-class PDFView(val pdfFile: Observable[Option[File]], val displayedPage: Observable[Int]) extends Component {
+class PDFView(val pdfFile: Observable[Option[File]], val displayedPage: Observable[Int]) extends Component with StrictLogging {
   minimumSize = new Dimension(300,400)
 
   private var file: Option[File] =  None
   private var page = 0
   //repaint if pdf or page changes
-  pdfFile.combineLatest(pdfFile.map(_ => 0) merge displayedPage)
-    .distinct
+  pdfFile.combineLatest(displayedPage)
+    .distinctUntilChanged
     .debounce(Duration("50ms")) //wait a bit until rendering the pdf
     .foreach{
     case (f,p) =>
       file = f
-      page = p
+      page = p - 1
+      logger.debug(s"changing pdf page to file $f page $p")
       this.repaint()
   }
 
@@ -44,21 +46,21 @@ class PDFView(val pdfFile: Observable[Option[File]], val displayedPage: Observab
   }
 }
 
-object PDFViewer{
+object PDFViewer extends StrictLogging {
   def newViewer(file: Observable[Option[File]]): MigPanel = {
+    file.foreach(f => logger.debug(s"changing file to $f"))
     new MigPanel{
-      val maxPage: Observable[Int] = file.map(_.map(getNumPages).getOrElse(0)).cache
+      val maxPage: Observable[Int] = file.map(_.map(getNumPages).getOrElse(0))
       val nextPage: Subject[Unit] = Subject[Unit]()
       val prevPage: Subject[Unit] = Subject[Unit]()
       val nextPageButton: Button = Button("→")(nextPage.onNext(()))
       val prevPageButton: Button = Button("←")(prevPage.onNext(()))
+      val turnPage: Observable[Int] = nextPage.map(_ => +1) merge prevPage.map(_ => -1)
       val page: Observable[Int] =
-        file
-          .map(_ => (nextPage.map(_ => +1) merge prevPage.map(_ => -1))
-            .scan(0)(_ + _)
-              .combineLatestWith(maxPage){case (p,max) => if(p < 0) 0 else if(p > max - 1) max - 1 else p})
-          .switch
-      val pageLabel = new RxLabel(page.combineLatest(maxPage).map{case (p,max) => s"${p+1}/$max"})
+        maxPage.switchMap(max =>
+          turnPage.scan(1){case (current,diff) => math.max(1,math.min(max,current + diff))}.distinctUntilChanged
+        )
+      val pageLabel = new RxLabel(page.combineLatest(maxPage).map{case (p,max) => s"$p/$max"})
       val viewer = new PDFView(file, page)
       add(prevPageButton, "split 3")
       add(pageLabel)
@@ -71,7 +73,7 @@ object PDFViewer{
 
   def getNumPages(file: File): Int = documentCache.get(file)(_.getNumberOfPages).getOrElse(0)
 
-  def renderPDFPage(file: File, page: Int, dpi: Int = 150): Image = {
+  def renderPDFPage(file: File, page: Int, dpi: Int = 90): Image = {
     documentCache.get(file)(pd =>
       pd.getDocumentCatalog.getAllPages.get(page).asInstanceOf[PDPage].convertToImage(BufferedImage.TYPE_BYTE_GRAY, dpi)
     ).getOrElse(new BufferedImage(1,1,BufferedImage.TYPE_BYTE_GRAY))
