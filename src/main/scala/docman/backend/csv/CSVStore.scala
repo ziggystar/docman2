ed9f539b5b0dc66a0a2ce83a39f9c43b44331cec
Thomas Vultura <ziggystar@gmx.de>
2019-07-28 09:55:12 +0200

    fix (CSV Backend): normalize paths to make comparison work

--- src/main/scala/docman/backend/csv/CSVStore.scala
+++ src/main/scala/docman/backend/csv/CSVStore.scala
@@ -13,7 +13,6 @@ import com.typesafe.scalalogging.StrictLogging
 import docman.core.{Document, DocumentStore}
 
 import scala.collection.JavaConverters._
-import scala.util.Try
 
 case class CSVStore(root: Path, dbFile: File) extends DocumentStore[EitherT[IO,String,?]] with StrictLogging {
   override type Content = File
@@ -22,17 +21,23 @@ case class CSVStore(root: Path, dbFile: File) extends DocumentStore[EitherT[IO,S
 
   var database: Map[Path, Document] = Map()
 
-  override def updateDocument(id: Id, d: Doc): EitherT[IO, String, Doc] = for {
-    _ <- EitherT.right(IO(logger.debug(s"update data for $id to $d")))
-    dUpdate <- EitherT.liftF(IO(d.copy(lastModified = LocalDateTime.now())))
-    _ <- CSVHelpers.write(dbFile, id, dUpdate)
-    _ <- EitherT.right(IO{database = database + (id -> dUpdate)})
-  } yield dUpdate
+  def normalizePath(p: Path): Path = root.toRealPath().relativize(p.toRealPath())
+
+  override def updateDocument(id: Id, d: Doc): EitherT[IO, String, Doc] = {
+    val realPath = normalizePath(id)
+    for {
+      _ <- EitherT.right(IO(logger.debug(s"update data for $realPath to $d")))
+      dUpdate <- EitherT.liftF(IO(d.copy(lastModified = LocalDateTime.now())))
+      _ <- CSVHelpers.write(dbFile, realPath, dUpdate)
+      _ <- EitherT.right(IO{database = database + (realPath -> dUpdate)})
+    } yield dUpdate
+  }
 
   override def reloadDB: EitherT[IO, String, Unit] = for{
     _ <- EitherT.right(IO(logger.debug(s"load database from file $dbFile")))
     entries <- CSVHelpers.readFile(dbFile, createIfNotExists = false)
-    _ <- EitherT.right(IO{database = entries.toMap})
+    normalized = entries.map(_.leftMap(normalizePath))
+    _ <- EitherT.right(IO{database = normalized.toMap})
   } yield ()
 
   override def scanForPDFs: EitherT[IO, String, Unit] = for{
@@ -46,11 +51,12 @@ case class CSVStore(root: Path, dbFile: File) extends DocumentStore[EitherT[IO,S
             .map(root.relativize)
             .filterNot(database.contains)
         ).attempt.map(_.leftMap(_.getMessage)))
-    _ <- newPDFs.map(f => updateDocument(f, Document())).toList.sequence
+    newPDFsNormalized = newPDFs.map(normalizePath)
+    _ <- newPDFsNormalized.map(f => updateDocument(f, Document())).toList.sequence
   } yield ()
 
   override def getAllDocuments: EitherT[IO, String, Seq[(Id, Doc)]] = EitherT.pure(database.toSeq)
 
-  override def access(id: Id): EitherT[IO, String, Content] = EitherT.pure(root.resolve(id).toFile)
+  override def access(id: Id): EitherT[IO, String, Content] = EitherT.pure(root.resolve(normalizePath(id)).toFile)
 }
 
